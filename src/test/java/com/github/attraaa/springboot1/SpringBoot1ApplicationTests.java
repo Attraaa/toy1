c9c8105a6bd9534994015e982e33ef81014baf11
Attraaa <sang8431k@naver.com>
2025-09-24 09:22:48 +0900

    first commit

---
+++ src/test/java/com/github/attraaa/springboot1/SpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.attraaa.springboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
